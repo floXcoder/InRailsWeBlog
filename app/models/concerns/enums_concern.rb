3630a8b7072b959a00d4b976fd96118fe94afa46
Flo <flo@l-x.fr>
2017-12-28 10:52:49 +0100

    Enum concern stability improved (Fixed #543)

--- app/models/concerns/enums_concern.rb
+++ app/models/concerns/enums_concern.rb
@@ -1,16 +1,16 @@
 module EnumsConcern
   extend ActiveSupport::Concern
 
-  VISIBILITY    = [:everyone, :only_me]
+  VISIBILITY    = [:everyone, :only_me].freeze
 
-  ERROR_ORIGIN  = [:server, :client, :communication]
+  ERROR_ORIGIN  = [:server, :client, :communication].freeze
 
   included do
     def self.enums_to_tr(klass, enums)
       enums.each do |enum|
         method_name = (enum.to_s + '_to_tr').to_sym
         send :define_method, method_name do
-          I18n.t(klass + '.enums.' + enum.to_s + '.' + self.send(enum))
+          self.send(enum) && I18n.t(klass + '.enums.' + enum.to_s + '.' + self.send(enum))
         end
       end
     end
