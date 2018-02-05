3af1a46d412061c4cceef81a5a48c0fde4d3c2b9
Flo <flo@l-x.fr>
2018-02-05 22:28:58 +0100

    Automatic deployment script updated

--- frontend/gulp/data.js
+++ frontend/gulp/data.js
@@ -1,6 +1,7 @@
 const gulp = require('gulp');
 const changed = require('gulp-changed');
-const browserSync = require('browser-sync');
+// const browserSync = require('browser-sync');
+
 const config = require('../config').data;
 
 // Move data to public directory
@@ -8,5 +9,5 @@ gulp.task('data', () =>
     gulp.src(config.src)
         .pipe(changed(config.dest)) // Ignore unchanged files
         .pipe(gulp.dest(config.dest))
-        .pipe(browserSync.reload({stream: true}))
+        // .pipe(browserSync.reload({stream: true}))
 );
--- frontend/gulp/fonts.js
+++ frontend/gulp/fonts.js
@@ -1,6 +1,7 @@
 const gulp = require('gulp');
 const changed = require('gulp-changed');
-const browserSync = require('browser-sync');
+// const browserSync = require('browser-sync');
+
 const config = require('../config').fonts;
 
 // Move fonts to public directory
@@ -8,5 +9,5 @@ gulp.task('fonts', () =>
     gulp.src(config.src)
         .pipe(changed(config.dest)) // Ignore unchanged files
         .pipe(gulp.dest(config.dest))
-        .pipe(browserSync.reload({stream: true}))
+        // .pipe(browserSync.reload({stream: true}))
 );
