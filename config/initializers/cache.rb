3729abffba9fc46738032cea72ba3f9a735e33dc
Flo <flo@l-x.fr>
2018-05-20 15:50:47 +0200

    Other JSON serializer tested but inconclusive (Fixed #593)

--- config/initializers/cache.rb
+++ config/initializers/cache.rb
@@ -1,1 +1,6 @@
 $redis = Redis::Namespace.new("_#{ENV['WEBSITE_NAME']}_#{Rails.env}:cache", redis: Redis.new(host: ENV['REDIS_HOST'], port: ENV['REDIS_PORT'], connect_timeout: 1, timeout: 1), warnings: false)
+
+# To display cache hit or miss
+# if Rails.en.production?
+#   Rails.cache.logger = Logger.new('log/cache.log')
+# end
