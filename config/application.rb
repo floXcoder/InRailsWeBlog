5f03e3925f1f2a52b3344a0ffddac63a61bdf9df
Flo <flo@l-x.fr>
2018-07-09 17:48:42 +0200

    Gitlab tool used for CI (5)

--- config/application.rb
+++ config/application.rb
@@ -11,11 +11,13 @@ require 'action_mailer/railtie'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-# preload tokens in application.yml to local ENV
-config = YAML.safe_load(File.read(File.expand_path('../application.yml', __FILE__)))
-config.merge! config.fetch(Rails.env, {})
-config.each do |key, value|
-  ENV[key] = value.to_s unless value.is_a? Hash
+# preload tokens in application.yml to local ENV unless variables are already defined (by Gitlab runner for instance)
+unless ENV['WEBSITE_NAME']
+  config = YAML.safe_load(File.read(File.expand_path('../application.yml', __FILE__)))
+  config.merge! config.fetch(Rails.env, {})
+  config.each do |key, value|
+    ENV[key] = value.to_s unless value.is_a? Hash
+  end
 end
 
 module InRailsWeBlog
