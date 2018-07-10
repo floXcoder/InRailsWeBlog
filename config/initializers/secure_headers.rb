563a88598dcf1a67517a57ba8ceb96f505e35f8e
Flo <flo@l-x.fr>
2018-07-10 17:45:58 +0200

    Dedicated domain used for assets and cookies free (2)

--- config/initializers/secure_headers.rb
+++ config/initializers/secure_headers.rb
@@ -5,7 +5,7 @@ SecureHeaders::Configuration.default do |config|
     default_src:      %w[localhost:8080 'self' ws: wss: assets.inrailsweblog.com *.l-x.fr],
     connect_src:      %w[localhost:8080 'self' ws: wss: assets.inrailsweblog.com *.l-x.fr],
     worker_src:       %w['self'],
-    script_src:       ['localhost:8080', "'self'", "'unsafe-inline'", "'unsafe-eval'", 'data:', ENV['METRICS_ADDRESS'], ENV['NEWRELIC_ADDRESS']],
+    script_src:       ['localhost:8080', "'self'", "'unsafe-inline'", "'unsafe-eval'", 'data:', ENV['WEBSITE_ASSET'], ENV['METRICS_ADDRESS'], ENV['NEWRELIC_ADDRESS']],
     img_src:          %w[* data:],
     font_src:         %w[localhost:8080 'self' assets.inrailsweblog.com *.l-x.fr data:],
     media_src:        %w[localhost:8080 'self' assets.inrailsweblog.com *.l-x.fr],
