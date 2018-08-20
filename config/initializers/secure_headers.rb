8a80d408532b10fcfd10b6a3510170db09efabc2
Flo <flo@l-x.fr>
2018-08-20 16:45:45 +0200

    Add new relic second address to secure headers

--- config/initializers/secure_headers.rb
+++ config/initializers/secure_headers.rb
@@ -5,7 +5,7 @@ SecureHeaders::Configuration.default do |config|
     default_src:      ['localhost:8080', "'self'", 'ws:', 'wss:', ENV['WEBSITE_ASSET']],
     connect_src:      ['localhost:8080', "'self'", 'ws:', 'wss:', ENV['WEBSITE_ASSET']],
     worker_src:       ["'self'"],
-    script_src:       ['localhost:8080', "'self'", "'unsafe-inline'", "'unsafe-eval'", 'data:', ENV['WEBSITE_ASSET'], ENV['METRICS_ADDRESS'], ENV['SENTRY_ADDRESS'], ENV['NEWRELIC_ADDRESS']],
+    script_src:       ['localhost:8080', "'self'", "'unsafe-inline'", "'unsafe-eval'", 'data:', ENV['WEBSITE_ASSET'], ENV['METRICS_ADDRESS'], ENV['SENTRY_ADDRESS'], ENV['NEWRELIC_ADDRESS'], ENV['NEWRELIC_SECOND_ADDRESS']],
     img_src:          ['*', 'data:'],
     font_src:         ['localhost:8080', "'self'", 'data:', ENV['WEBSITE_ASSET']],
     media_src:        ['localhost:8080', "'self'", ENV['WEBSITE_ASSET']],
