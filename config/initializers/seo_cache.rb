921a162a88a1edc83b802b24548bf8efba78735e
Flo <flo@l-x.fr>
2019-05-05 22:53:38 +0200

    Update SEO cache configuration

--- config/initializers/seo_cache.rb
+++ config/initializers/seo_cache.rb
@@ -3,19 +3,19 @@
 if Rails.env.production?
   require 'seo_cache'
 
-  SeoCache.cache_mode = 'disk'
-  SeoCache.disk_cache_path = Rails.root.join('public', 'seo_cache')
+  SeoCache.cache_mode = Rails.env.development? ? 'memory' : 'disk'
+  SeoCache.cache_path = Rails.root.join('public', 'seo_cache')
+
+  SeoCache.chrome_path = Rails.env.production? ? '/usr/bin/chromium' : '/usr/bin/chromium-browser'
 
   SeoCache.redis_url = "redis://#{ENV['REDIS_HOST']}:#{ENV['REDIS_PORT']}"
   SeoCache.redis_namespace =  "_#{ENV['WEBSITE_NAME']}_#{Rails.env}:seo_cache"
 
   SeoCache.blacklist_urls = %w[^/admin.*]
-
   SeoCache.blacklist_params = %w[page locale]
 
+  SeoCache.logger_path = Rails.root.join('log', 'seo_cache.log')
   SeoCache.log_missed_cache = true
-
-  # SeoCache.prerender_service_url = ENV['PRERENDER_SERVICE_URL'] || 'http://localhost:3010'
 
   Rails.application.config.middleware.use SeoCache::Middleware
 end
