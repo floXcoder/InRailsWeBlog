a60680adb32ec9135c9ef31f0dd6d882b02dc0be
Flo <flo@l-x.fr>
2020-05-11 22:08:15 +0200

    Add database indexes to improve performance when fetching articles

---
+++ db/migrate/20200511195952_optimize_article_indexes.rb
@@ -0,0 +1,9 @@
+class OptimizeArticleIndexes < ActiveRecord::Migration[6.0]
+  def change
+    add_index :articles, :deleted_at
+    add_index :articles, :visibility, where: 'deleted_at IS NULL'
+
+    commit_db_transaction
+    add_index :versions, [:item_id], algorithm: :concurrently
+  end
+end
