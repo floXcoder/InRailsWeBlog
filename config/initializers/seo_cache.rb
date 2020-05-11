77c7b9c6d22d5ae31733d7cf8d7e51b1c7abaa94
Flo <flo@l-x.fr>
2020-05-11 15:21:34 +0200

    Exclude forbidden URLs extension from SEO cache

--- config/initializers/seo_cache.rb
+++ config/initializers/seo_cache.rb
@@ -17,7 +17,7 @@ if Rails.env.production?
   SeoCache.redis_url = "redis://#{ENV['REDIS_HOST']}:#{ENV['REDIS_PORT']}"
   SeoCache.redis_namespace =  "_#{ENV['WEBSITE_NAME']}_#{Rails.env}:seo_cache"
 
-  SeoCache.blacklist_urls = %w[^/admin.*]
+  SeoCache.blacklist_urls = %w[^/admin.* \.html$ \.htm$ \.php$ \.gz$ \.xml$]
   SeoCache.blacklist_params = %w[page locale]
 
   SeoCache.wait_time_for_page_loading = 3
