8e7baaff6065bcff7eaa5a6c747d3c8101df8a2d
Flo <flo@l-x.fr>
2021-09-23 09:31:35 +0200

    Hide Redis warning

--- config/initializers/cache.rb
+++ config/initializers/cache.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+Redis.exists_returns_integer = true
+
 $redis = Redis::Namespace.new("_#{ENV['WEBSITE_NAME']}_#{Rails.env}:cache", redis: Redis.new(host: ENV['REDIS_HOST'], port: ENV['REDIS_PORT'], connect_timeout: 1, timeout: 1), warnings: false)
 
 # To display cache hit or miss
