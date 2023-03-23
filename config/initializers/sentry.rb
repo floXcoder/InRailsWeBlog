a5bde355c40ec14a7175b51c7bfcca2b56f1a3bd
Flo <flo@l-x.fr>
2023-03-23 11:57:11 +0100

    Add replay to Sentry

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -10,6 +10,6 @@ if Rails.env.production? && ENV['SENTRY_RAILS_KEY']
 
     # Set traces_sample_rate to 1.0 to capture 100% of transactions for performance monitoring.
     # We recommend adjusting this value in production.
-    config.traces_sample_rate = 1.0
+    config.traces_sample_rate = 0
   end
 end
